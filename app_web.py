4ec0ed05530a7cb342a91ee215692df5b589d4df
Damian Delfino <102768719+DamiDelfino@users.noreply.github.com>
2026-05-27 12:40:01 -0300

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -59,7 +59,7 @@ def dibujar_cancha(equipo, titulo, color_puntos):
 # 3. INTERFAZ WEB
 # ==========================================
 st.set_page_config(page_title="Fútbol 8 Pro", page_icon="⚽", layout="wide")
-st.title("⚽ Armador de Equipos EA FC")
+st.title("⚽ Armador de Equipos Piraña")
 
 try:
     conn = conectar_db()
